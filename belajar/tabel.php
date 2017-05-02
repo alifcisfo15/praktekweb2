c2a8e281b814020e02da416e08ad155ad39d1e36
alif <juno7alif@gmail.com>
2017-05-02 11:39:28 +0700

    Ini loh pak

---
+++ belajar/tabel.php
@@ -0,0 +1,18 @@
+<table border="1">
+	<tr>
+		<td>No</td>
+		<td>NIM</td>
+		<td>Nama</td>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>H1101151007</td>
+		<td>Alif Fitrah</td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>H1101151090</td>
+		<td>Herjunot Ali</td>
+	</tr>
+
+</table>
